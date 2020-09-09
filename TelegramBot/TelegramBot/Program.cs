f25e5fefffa7279bebe1360ed3d0f437fb30e59d
Volodymyr Rothenbert <vovkawool@outlook.com>
2020-09-09 16:18:48 +0300

    first commit

---
+++ TelegramBot/TelegramBot/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Newtonsoft.Json.Linq;
+using System.Collections.Specialized;
+
+namespace TelegramBot
+{
+    class Program
+    {
+        static void CheckNumber(int number, ref bool statusNum)
+        {
+            string url = $"https://localhost:44302/api/numbers/{number}";
+            var json = new WebClient() { Encoding = Encoding.UTF8 }.DownloadString(url);
+
+            statusNum = json.Equals("true") ? true : false;
+
+        }
+        static void Main(string[] args)
+        {
+            bool statusNumber = true;
+
+            TelegramBotClient bot = new TelegramBotClient("1331063915:AAEnGb1XocJod9d6digo_q7hlnkAxpKp8R4");
+            bot.OnMessage += (s, arg) =>
+            {
+                try
+                {
+                    Console.WriteLine($"{arg.Message.Chat.FirstName}: {arg.Message.Text}");
+                    CheckNumber(Int32.Parse(arg.Message.Text), ref statusNumber);
+                    bot.SendTextMessageAsync(arg.Message.Chat.Id, $"Статус вашего числа: {statusNumber}");
+                }
+                catch
+                {
+                    bot.SendTextMessageAsync(arg.Message.Chat.Id, $"{arg.Message.Chat.FirstName}, TU DYRAK");
+                }       
+            };
+            
+            bot.StartReceiving();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ WebApiApp/WebApiApp/Controllers/NumbersController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApiApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NumbersController : ControllerBase
+    {
+        List<int> numbersList = new List<int>() { 1, 2, 3, 15, 19, 7, 8 ,13, 16 ,10 };
+
+        // GET api/numbers
+        [HttpGet]
+        public ActionResult<IEnumerable<int>> GetNumbers()
+        {
+            return numbersList;
+        }
+
+        [HttpGet("{arg}")]
+        public ActionResult<bool> IsExist(int arg)
+        {
+            return (numbersList.Contains(arg));
+        }
+    }
+}
